90d420c09f9050a79769eb4f648f35fb5f72e759
Claudio Rodriguez Herrera <claudiorh1953@gmail.com>
2023-12-15 20:57:39 -0300

    Refactor code to use 'with' statement for file handling

--- Challenge#01/Challenge_01.py
+++ Challenge#01/Challenge_01.py
@@ -1,9 +1,9 @@
-f = open("Challenge#01/message_01.txt", "r")
-f = list(f)
 text = []
-for word in f:
-    word = word.lower()
-    text += word.split(" ")
+with open("Challenge#01/message_01.txt", "r") as f:
+    for word in f:
+        word = word.lower()
+        text += word.split(" ")
+
 result = []
 response = ""
 for word in text:
